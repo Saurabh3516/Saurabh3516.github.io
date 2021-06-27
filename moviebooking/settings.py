d8454beb0354bac947e76106290336b1a3fc0c45
Saurabh3516 <55134877+Saurabh3516@users.noreply.github.com>
2021-06-27 20:41:31 +0530

    Static file changes

--- moviebooking/settings.py
+++ moviebooking/settings.py
@@ -125,7 +125,7 @@ STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
-    'static',
+    os.path.join(BASE_DIR, 'static'),
 ]
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
